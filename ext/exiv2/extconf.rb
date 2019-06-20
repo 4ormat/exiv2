c004159b510f0775e410d35d422fb2141bd218a4
Mike Ferrier <mferrier@gmail.com>
2019-06-20 18:13:54 -0400

    remove stuff we don't want

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -5,8 +5,6 @@ RbConfig::MAKEFILE_CONFIG['CCFLAGS'] = ENV['CCFLAGS'] if ENV['CCFLAGS']
 RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
 RbConfig::MAKEFILE_CONFIG['CXXFLAGS'] = ENV['CXXFLAGS'] if ENV['CXXFLAGS']
 
-dir_config("exiv2")
-have_library("exiv2")
 config = pkg_config('exiv2')
 if config
   $CPPFLAGS = [$CPPFLAGS, config[0]].join ' '
